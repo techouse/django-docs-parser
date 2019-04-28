a06c8b558c6d23a4236be4bfcf534ed3e6cecfba
Klemen Tušar <techouse@gmail.com>
2019-04-28 15:53:25 +0200

    Add Django 2.2 and remove Django 2.0

--- parser.py
+++ parser.py
@@ -9,103 +9,149 @@ from zipfile import ZipFile
 import requests
 from bs4 import BeautifulSoup
 
-DJANGO_VERSIONS = ('2.1', '2.0', '1.11')
-DJANGO_DOCS_DOWNLOAD_URL = 'https://docs.djangoproject.com/m/docs/django-docs-{version}-en.zip'
-DJANGO_DOCS_URL = 'https://docs.djangoproject.com/en/{version}/'
+DJANGO_VERSIONS = ("2.2", "2.1", "1.11")
+DJANGO_DOCS_DOWNLOAD_URL = (
+    "https://docs.djangoproject.com/m/docs/django-docs-{version}-en.zip"
+)
+DJANGO_DOCS_URL = "https://docs.djangoproject.com/en/{version}/"
 
-EXCLUDED_IDS = {'term-', 'allow_migrate-', 'allow_relation-', 'index-'}
+EXCLUDED_IDS = {"term-", "allow_migrate-", "allow_relation-", "index-"}
 
 
 def parse():
     entries = []
 
     for version in DJANGO_VERSIONS:
-        with ZipFile(BytesIO(requests.get(DJANGO_DOCS_DOWNLOAD_URL.format(version=version)).content)) as zipfile:
-            with zipfile.open('genindex.html') as genindex:
-                index_soup = BeautifulSoup(genindex, 'lxml')
+        with ZipFile(
+            BytesIO(
+                requests.get(DJANGO_DOCS_DOWNLOAD_URL.format(version=version)).content
+            )
+        ) as zipfile:
+            with zipfile.open("genindex.html") as genindex:
+                index_soup = BeautifulSoup(genindex, "lxml")
 
-                for index, table in enumerate(index_soup.find_all('table', class_='genindextable')):
+                for index, table in enumerate(
+                    index_soup.find_all("table", class_="genindextable")
+                ):
                     if index < 2:
                         # skip first 2 tables listing Symbols and dunder methods
                         continue
 
-                    for a in table.find_all('a'):
-                        href = a.get('href')
-                        file, id = href.split('#')
+                    for a in table.find_all("a"):
+                        href = a.get("href")
+                        file, id = href.split("#")
 
-                        if file.startswith('releases/') or any(id.startswith(pattern) for pattern in EXCLUDED_IDS):
+                        if file.startswith("releases/") or any(
+                            id.startswith(pattern) for pattern in EXCLUDED_IDS
+                        ):
                             continue
 
                         with zipfile.open(file) as html:
                             try:
-                                topic_soup = BeautifulSoup(html, 'lxml')
+                                topic_soup = BeautifulSoup(html, "lxml")
                                 topic = topic_soup.find(id=id)
                                 parent = topic.parent
 
                                 entry_data = {
-                                    'version': float(version),
-                                    'id': id.replace('std:', '') if id.startswith('std:') else id,
-                                    'title': '',
-                                    'permalink': urljoin(DJANGO_DOCS_URL.format(version=version),
-                                                         href.replace('.html', '/')),
-                                    'categories': [],
-                                    'content': ''
+                                    "version": float(version),
+                                    "id": id.replace("std:", "")
+                                    if id.startswith("std:")
+                                    else id,
+                                    "title": "",
+                                    "permalink": urljoin(
+                                        DJANGO_DOCS_URL.format(version=version),
+                                        href.replace(".html", "/"),
+                                    ),
+                                    "categories": [],
+                                    "content": "",
                                 }
 
-                                if parent.name == 'dl' and topic.name == 'dt':
+                                if parent.name == "dl" and topic.name == "dt":
                                     # set the title
                                     if any(character.isupper() for character in id):
-                                        entry_data['title'] = id[re.search('[A-Z]', id).start():]
+                                        entry_data["title"] = id[
+                                            re.search("[A-Z]", id).start() :
+                                        ]
                                     else:
-                                        if topic.find(class_='descclassname'):
-                                            entry_data['title'] += topic.find(class_='descclassname').get_text()
-                                        if topic.find(class_='descname'):
-                                            entry_data['title'] += topic.find(class_='descname').get_text()
-                                        if parent.get('class') == 'glossary docutils':
-                                            entry_data['title'] += topic.get_text()
+                                        if topic.find(class_="descclassname"):
+                                            entry_data["title"] += topic.find(
+                                                class_="descclassname"
+                                            ).get_text()
+                                        if topic.find(class_="descname"):
+                                            entry_data["title"] += topic.find(
+                                                class_="descname"
+                                            ).get_text()
+                                        if parent.get("class") == "glossary docutils":
+                                            entry_data["title"] += topic.get_text()
                                     # set the content
-                                    if parent.find('dd').find('p'):
-                                        entry_data['content'] = ' '.join(parent.find('dd').find('p').get_text().split())
+                                    if parent.find("dd").find("p"):
+                                        entry_data["content"] = " ".join(
+                                            parent.find("dd")
+                                            .find("p")
+                                            .get_text()
+                                            .split()
+                                        )
                                     # set the categories
-                                    if topic.find(class_='property'):
-                                        entry_data['categories'].append(topic.find(class_='property').get_text())
-                                    if id.startswith('django.'):
-                                        entry_data['categories'] += id.split('.')[1:-1]
+                                    if topic.find(class_="property"):
+                                        entry_data["categories"].append(
+                                            topic.find(class_="property").get_text()
+                                        )
+                                    if id.startswith("django."):
+                                        entry_data["categories"] += id.split(".")[1:-1]
                                 else:
-                                    if id.startswith('std:'):
-                                        if ':setting-' in id:
+                                    if id.startswith("std:"):
+                                        if ":setting-" in id:
                                             # set the title
-                                            entry_data['title'] = id.split('-')[-1]
+                                            entry_data["title"] = id.split("-")[-1]
                                             # set the categories
-                                            entry_data['categories'].append(re.split(r'[:\-]', id)[1])
+                                            entry_data["categories"].append(
+                                                re.split(r"[:\-]", id)[1]
+                                            )
                                             # set te content
-                                            if len(parent.find_all('p')) > 1:
-                                                entry_data['content'] = ' '.join(
-                                                    parent.find_all('p')[1].get_text().split())
+                                            if len(parent.find_all("p")) > 1:
+                                                entry_data["content"] = " ".join(
+                                                    parent.find_all("p")[1]
+                                                    .get_text()
+                                                    .split()
+                                                )
                                             else:
-                                                entry_data['content'] = ' '.join(parent.find('p').get_text().split())
-                                        elif ':templatefilter-' in id:
+                                                entry_data["content"] = " ".join(
+                                                    parent.find("p").get_text().split()
+                                                )
+                                        elif ":templatefilter-" in id:
                                             # set the title
-                                            entry_data['title'] = id.split('-')[-1]
+                                            entry_data["title"] = id.split("-")[-1]
                                             # set the categories
-                                            entry_data['categories'].append(re.split(r'[:\-]', id)[1])
+                                            entry_data["categories"].append(
+                                                re.split(r"[:\-]", id)[1]
+                                            )
                                             # set te content
-                                            entry_data['content'] = ' '.join(parent.find('p').get_text().split())
-                                        elif ':fieldlookup-' in id:
+                                            entry_data["content"] = " ".join(
+                                                parent.find("p").get_text().split()
+                                            )
+                                        elif ":fieldlookup-" in id:
                                             # set the title
-                                            entry_data['title'] = id.split('.')[-1]
+                                            entry_data["title"] = id.split(".")[-1]
                                             # set the categories
-                                            entry_data['categories'].append(re.split(r'[:\-.]', id)[1])
+                                            entry_data["categories"].append(
+                                                re.split(r"[:\-.]", id)[1]
+                                            )
                                             # set te content
-                                            if parent.find('p'):
-                                                entry_data['content'] = ' '.join(parent.find('p').get_text().split())
-                                        elif ':templatetag-' in id:
+                                            if parent.find("p"):
+                                                entry_data["content"] = " ".join(
+                                                    parent.find("p").get_text().split()
+                                                )
+                                        elif ":templatetag-" in id:
                                             # set the title
-                                            entry_data['title'] = id.split('-')[-1]
+                                            entry_data["title"] = id.split("-")[-1]
                                             # set the categories
-                                            entry_data['categories'].append(re.split(r'[:\-]', id)[1])
+                                            entry_data["categories"].append(
+                                                re.split(r"[:\-]", id)[1]
+                                            )
                                             # set te content
-                                            entry_data['content'] = ' '.join(parent.find('p').get_text().split())
+                                            entry_data["content"] = " ".join(
+                                                parent.find("p").get_text().split()
+                                            )
 
                                 entries.append(entry_data)
                             except:
@@ -114,9 +160,9 @@ def parse():
                                 raise
 
     if entries:
-        with open(os.path.abspath('data.json'), 'w') as fh:
+        with open(os.path.abspath("data.json"), "w") as fh:
             json.dump(entries, fh, indent=4)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parse()
