7d3d9a3d7289457238b865d9a55aaadbbf26ca31
Klemen Tušar <techouse@gmail.com>
2020-12-31 11:33:47 +0000

    :sparkles: Support these versions 3.1, 3.0, 2.2, 2.1, 2.0, 1.11, 1.10, 1.8

--- parser.py
+++ parser.py
@@ -9,7 +9,7 @@ from zipfile import ZipFile
 import requests
 from bs4 import BeautifulSoup
 
-DJANGO_VERSIONS = ("3.1", "2.2", "1.11")
+DJANGO_VERSIONS = ("3.1", "3.0", "2.2", "2.1", "2.0", "1.11", "1.10", "1.8")
 DJANGO_DOCS_DOWNLOAD_URL = (
     "https://docs.djangoproject.com/m/docs/django-docs-{version}-en.zip"
 )
